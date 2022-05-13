61d9bf15d0393afbc0e9f1aa930738696397a200
Rumata-SL <kirr7619@gmail.com>
2022-05-14 03:06:49 +0500

    Changed UsersContainer, deleted components Users.tsx, UsersApiComponent.tsx

--- src/Components/Users/UsersContainer.tsx
+++ src/Components/Users/UsersContainer.tsx
@@ -8,8 +8,56 @@ import {
     UnFollowAC,
     UsersType,
 } from "../../Redux/UsersReducer";
-import {Users} from "./Users";
-import {UsersApiComponent} from "./UsersApiComponent";
+import axios from "axios";
+import {UsersPresentation} from "./UsersPresentation";
+
+
+type UsersApiContainerPropsType = {
+    items: Array<UsersType>
+    follow: (userId: number) => void
+    unfollow: (userId: number) => void
+    setUsers: (user: Array<UsersType>) => void
+    setCurrentPage: (currentPage: number) => void
+    setTotalUsersCount: (totalUsersCount: number) => void
+    totalUsersCount: number
+    pageSize: number
+    currentPage: number
+}
+
+
+export class UsersApiContainer extends React.Component<UsersApiContainerPropsType> {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
+    }
+
+    onPageChanged = (pageNumber: number) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            })
+    }
+
+    render() {
+
+        return <UsersPresentation
+            totalUsersCount={this.props.totalUsersCount}
+            pageSize={this.props.pageSize}
+            items={this.props.items}
+            follow={this.props.follow}
+            unfollow={this.props.unfollow}
+            onPageChanged={this.onPageChanged}
+            currentPage={this.props.currentPage}
+        />
+
+    }
+}
+
 
 
 const mapStateToProps = (state: AppStoreType) => {
@@ -41,4 +89,4 @@ const mapDispatchToProps = (dispatch: Dispatch) => {
 
 }
 
-export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersApiComponent)
+export const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersApiContainer)
