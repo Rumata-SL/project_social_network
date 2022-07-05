07b4c942286cd3445ae29e950aadbd6e7cb48a62
Rumata-SL <kirr7619@gmail.com>
2022-07-06 00:39:46 +0500

    changed WithSuspense

--- src/Hoc/WithSuspense.tsx
+++ src/Hoc/WithSuspense.tsx
@@ -2,22 +2,23 @@ import React, {ComponentType} from "react";
 import {Preloader} from "../common/preloader/Preloaded";
 
 
-/*export function withSuspense<WCP>(WrappedComponent: React.ComponentType<WCP>) {
+/*export function withSuspense<WCP>(Wrapper: React.ComponentType<WCP>) {
     return (props: any) => {
         let {...restProps} = props
         return (
             <React.Suspense fallback={<Preloader/>}>
-                <WrappedComponent {...restProps as WCP}/>
+                <Wrapper {...restProps as WCP}/>
             </React.Suspense>
         )
     }
 }*/
 
 
-export function withSuspense<T>(Component: ComponentType<T>) {
+export function withSuspense<T>(Wrapper: ComponentType<T>) {
     return (props: any) => {
+    let {...restProps} = props
         return <React.Suspense fallback={<Preloader/>}>
-            <Component {...props}/>
+            <Wrapper {...restProps}/>
         </React.Suspense>
     }
 }
